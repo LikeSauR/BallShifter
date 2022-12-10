27b1c6fb5b734779cbb1244e518d0192d4b4d05a
LikeSauR <popadynetsalexander@gmail.com>
2022-12-10 13:55:09 +0200

    Scripts Update v2

---
+++ Scripts/Door.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float seconds;
+    //[HideInInspector] 
+    public float target, progress;
+    [System.Serializable]
+    public class Objective
+    {
+        public Transform obj;
+        public Vector3 posA, posB;
+    }
+    public Objective[] objectives;
+    void Update()
+    {
+        progress = Mathf.Lerp(progress, target, Time.deltaTime / seconds);
+        foreach (var o in objectives)
+        {
+            o.obj.position = Vector3.Lerp(o.posA, o.posB, progress);
+        }
+    }
+    void OnDrawGizmos()
+    {
+        if (objectives != null)
+        foreach (var o in objectives)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(o.posA, .25f);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(o.posB, .25f);
+        }
+    }
+}
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -8,16 +8,23 @@ public class Player : MonoBehaviour
     Rigidbody rb;
     public float speed;
     public Transform rotorX, rotorY, cam;
+    public Vector2 sensitivity;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        
+        UpdateCamera();
         rb.AddForce(speed * Time.deltaTime * (
-            rotorX.forward * Input.GetAxis("Vertical") + 
+            rotorX.forward * Input.GetAxis("Vertical") +
             rotorX.right * Input.GetAxis("Horizontal")
         ));
+    }
+    void UpdateCamera()
+    {
+        rotorX.position = Vector3.Lerp(rotorX.position, transform.position, 10f * Time.deltaTime);
+        rotorX.eulerAngles += new Vector3(0, Input.GetAxis("Mouse X") * sensitivity.x);
+        //rotorY.localEulerAngles = new Vector3(rotorY.localEulerAngles + Input.GetAxis("Mouse Y") * sensitivity.x ,0);
     }
 }
