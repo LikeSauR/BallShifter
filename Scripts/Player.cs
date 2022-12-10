24cda5e766f4117f4838701879b60832bf9e5e51
LikeSauR <popadynetsalexander@gmail.com>
2022-12-10 13:38:10 +0200

    Scripts Upload v1

---
+++ Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    public Transform rotorX, rotorY, cam;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        
+        rb.AddForce(speed * Time.deltaTime * (
+            rotorX.forward * Input.GetAxis("Vertical") + 
+            rotorX.right * Input.GetAxis("Horizontal")
+        ));
+    }
+}
